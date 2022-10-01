621f2342eb1e348abc212b28c306fe5438bae71f
Vo Ngoc Nhat Quang <quangvip2611@gmail.com>
2022-10-01 18:34:52 +0700

    First commit

---
+++ 2022-09-14/src/TrenLop/GuiNhan.java
@@ -0,0 +1,47 @@
+package TrenLop;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author quang
+ */
+public class GuiNhan {
+    ServerSocket Sserver;
+    
+    public void MoCong(int cong) throws Exception{
+        Sserver = new ServerSocket(cong);
+        System.out.println("Server da mo cong: " + cong);
+    }
+    
+    public String Nhan() throws Exception{
+        //Lap vo han de doi client noi len
+        Socket c=   Sserver.accept();
+        System.out.println("Da co 1 client connected ........");
+        //Tạo luồng để nhận thông tin client gửi lên
+        InputStreamReader is= new InputStreamReader(c.getInputStream());
+         //Tạo bộ đệm để lưu dữ liệu nhận về
+          BufferedReader r= new BufferedReader(is);
+          return r.readLine(); //đọc dữ liệu từ bộ đệm
+    } //
+    
+    public void Gui(String data, String Ipserver, int cong) throws Exception{
+        Socket c = new Socket(Ipserver, cong);
+        System.out.println("Da noi len Server........");
+        //Tạo luồng để gửi dũ  liệu lên Server
+        DataOutputStream out =new DataOutputStream(c.getOutputStream());
+        out.writeBytes(data); //Gửi thông báo
+        out.write(13);
+        out.write(10);
+        out.close();
+        c.close();
+    }
+}
